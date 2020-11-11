6b53188d5618a7962fee843c7c3869a9ce76fcae
funyeatsalad <73189162+funyeatsalad@users.noreply.github.com>
2020-11-11 00:49:09 -0600

    sorry

---
+++ src/sorry i fixed it again.cs
@@ -0,0 +1,11 @@
+using System;
+// sorry i didnt know i mispelt pulic!
+public class Program
+{
+  public static string word = "Hello Gus!";
+  
+	public static void Main()
+	{
+		Console.WriteLine(word);
+	}
+}
